3700d39fcda95199d8c53ad08404c3347b3b8646
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-10 17:48:25 +0300

    Added route to book appointment

--- routes/PetOwnerRoutes.js
+++ routes/PetOwnerRoutes.js
@@ -11,5 +11,6 @@ router.use(checkAuthMiddleware.checkAuthPetOwner);
 // Specific to pet owners
 
 router.get("/myProfile", PetOwnerController.getmyProfile);
+router.get("/appointment", PetOwnerController.bookAppointment);
 
 module.exports = router;
