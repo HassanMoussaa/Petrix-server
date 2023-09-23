9e4ae945f142e3aff3ca4a910772beda0f8426f4
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-23 16:56:08 +0300

    Updated  send notification controller

--- controllers/FCMController.js
+++ controllers/FCMController.js
@@ -7,22 +7,29 @@ admin.initializeApp({
 });
 
 async function sendNotification(req, res, next) {
-  let { doctor_name, pet_owner_name, app_date, app_time } =
-    req.notificationInfo;
+  let {
+    doctor_name,
+    pet_owner_name,
+    app_date,
+    app_time,
+    doc_id,
+    notification_type,
+  } = req.notificationInfo;
 
-  const user_id = req.userData.user_id;
+  let title = "";
+  let body = "";
+  let query_tokens = [];
 
-  // setting the notification content
-  let title = "New Appointment Request!";
-  let body = `${pet_owner_name} requested an appointment on ${app_date} at ${app_time}!`;
-  let query_tokens = await FirebaseToken.findAll({
-    attributes: ["token"],
-    where: { user_id: user_id },
-  });
+  if (notification_type == "new_booking_notification") {
+    title = "New Appointment Request!";
+    body = `${pet_owner_name} requested an appointment on ${app_date} at ${app_time}!`;
+    query_tokens = await FirebaseToken.findAll({
+      attributes: ["token"],
+      where: { user_id: doc_id },
+    });
+  }
 
   const tokens = query_tokens?.map((tokenObj) => tokenObj.dataValues.token);
-
-  console.log("TOKENS1:: ", t);
 
   let message = {
     tokens,
@@ -37,11 +44,11 @@ async function sendNotification(req, res, next) {
     .sendMulticast(message)
     .then((response) => {
       console.log("Notification sent:", response);
-      res.json(response); // Send a response if needed
+      res.json(response);
     })
     .catch((error) => {
       console.error("Error sending notification:", error);
-      next(error); // Pass the error to the next middleware or handler
+      next(error);
     });
 }
 
