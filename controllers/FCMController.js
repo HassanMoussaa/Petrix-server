a16cd5fdf172c4630a172364b43435df081c03da
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-23 18:32:30 +0300

    Updated fcm conttroller to handle accept-reject appointments

--- controllers/FCMController.js
+++ controllers/FCMController.js
@@ -13,6 +13,7 @@ async function sendNotification(req, res, next) {
     app_date,
     app_time,
     doc_id,
+    petOwner_id,
     notification_type,
   } = req.notificationInfo;
 
@@ -23,9 +24,26 @@ async function sendNotification(req, res, next) {
   if (notification_type == "new_booking_notification") {
     title = "New Appointment Request!";
     body = `${pet_owner_name} requested an appointment on ${app_date} at ${app_time}!`;
+    console.log("hi");
     query_tokens = await FirebaseToken.findAll({
       attributes: ["token"],
       where: { user_id: doc_id },
+    });
+  } else if (notification_type == "accepted_appointment_notification") {
+    title = "Appointment Accepted!";
+    body = `You appointment with Dr. ${doctor_name} on  ${app_date} at ${app_time} has been accepted!`;
+    console.log("bye");
+    query_tokens = await FirebaseToken.findAll({
+      attributes: ["token"],
+      where: { user_id: petOwner_id },
+    });
+  } else if (notification_type == "rejected_appointment_notification") {
+    title = "Appointment Rejected!";
+    body = `You appointment with Dr. ${doctor_name} on  ${app_date} at ${app_time} has been rejected!`;
+    console.log("bye");
+    query_tokens = await FirebaseToken.findAll({
+      attributes: ["token"],
+      where: { user_id: petOwner_id },
     });
   }
 
