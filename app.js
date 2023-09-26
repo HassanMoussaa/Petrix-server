198cf385c44c2694803f05cf7b902ee5307b27c3
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-26 10:05:50 +0300

    Initialized chat feature in the main app js file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const doctorRoutes = require("./routes/DoctorRoutes");
 const petOwnerRoutes = require("./routes/PetOwnerRoutes");
 const userRoutes = require("./routes/UserRoutes");
+const chatRoutes = require("./routes/ChatRoute");
 
 const app = express();
 
@@ -21,6 +22,8 @@ app.use("/pet_pictures", express.static("images/pet_pictures"));
 app.use("/doctors", doctorRoutes);
 app.use("/petOwners", petOwnerRoutes);
 app.use("/users", userRoutes);
+app.use("/chat", chatRoutes);
+
 app.listen(8000, () => {
   console.log("Listening on Port 8000:");
 });
